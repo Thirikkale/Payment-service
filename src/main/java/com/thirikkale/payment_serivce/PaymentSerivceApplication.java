2028be4fc418fbbc4b7bcce0c569ec8f56ccac83
nuran912 <nuranalwis@gmail.com>
2025-10-18 00:52:32 +0530

    initialization

---
+++ src/main/java/com/thirikkale/payment_serivce/PaymentSerivceApplication.java
@@ -0,0 +1,13 @@
+package com.thirikkale.payment_serivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSerivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSerivceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thirikkale/payment_serivce/PaymentSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thirikkale.payment_serivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
